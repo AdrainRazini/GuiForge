c8204c0a5e5920bc6ce640dd81b010dc185c3a3a
Adrian Razini Rangel <160901383+AdrainRazini@users.noreply.github.com>
2025-09-29 02:21:14 +0000

    Comit Uix Regui

---
+++ Modules/Regui.lua
@@ -0,0 +1,23 @@
+
+local Regui
+-- Tenta carregar localmente
+local success, module = pcall(function()
+	return require(script.Parent:FindFirstChild("Mod_UI"))
+end)
+
+if success and module then
+	Regui = module
+else
+	-- Tenta baixar remoto
+	local HttpService = game:GetService("HttpService")
+	local ok, err = pcall(function()
+		local code = game:HttpGet("https://raw.githubusercontent.com/AdrainRazini/mastermod/refs/heads/main/module/dataGui.lua")
+		Regui = loadstring(code)()
+	end)
+
+	if not ok then
+		warn("Não foi possível carregar Mod_UI nem local nem remoto!", err)
+	end
+end
+
+assert(Regui, "Regui não foi carregado!")
